4c2bc55df81f84b7b2c5deb96edb5a05f62c8d87
Jack Snowdon <jacksnowdondrums@gmail.com>
2019-04-25 16:26:22 +0000

    updated requirements and added templates folder to demostrate connection to mongodb

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -8,9 +8,12 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'dnd_project3'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tuser@myfirstcluster-rxis3.mongodb.net/dnd_project3?retryWrites=true'
 
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World ... again'
+@app.route('/browse_spells')
+def browse_spells():
+    return render_template("spells.html", spells=mongo.db.spells.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
