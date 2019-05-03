04737c269511d876cf5fce393cfd87a4a26f0cb6
Jack Snowdon <jacksnowdondrums@gmail.com>
2019-05-03 09:55:14 +0000

    added config vars, runs locally currently as true

--- app.py
+++ app.py
@@ -5,8 +5,21 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'dnd_project3'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tuser@myfirstcluster-rxis3.mongodb.net/dnd_project3?retryWrites=true'
+# Debugger for running locally, change to False when pushing to heroku
+
+app.config['DEBUG'] = True
+#app.config["MONGO_DBNAME"] = 'dnd_project3'
+#app.config["MONGO_URI"] = 'mongodb+srv://root:r00tuser@myfirstcluster-rxis3.mongodb.net/dnd_project3?retryWrites=true'
+
+# If statement to ensure env vars are set running locally or on heroku
+
+if app.config["DEBUG"] == True:
+    import config
+    app.config['MONGO_URI'] = config.MONGO_URI
+    app.config['DB_NAME'] = config.DB_NAME
+else:
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    app.config["DB_NAME"] = os.getenv("DB_NAME")
 
 mongo = PyMongo(app)
 
@@ -17,6 +30,5 @@ def browse_spells():
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+        port=int(os.environ.get('PORT')))
         
