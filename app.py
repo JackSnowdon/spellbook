3f4632714ecb36b5400c4586b68e3be564c27597
Jack Snowdon <jacksnowdondrums@gmail.com>
2019-05-03 10:03:13 +0000

    Refactored debug code

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 
 # Debugger for running locally, change to False when pushing to heroku
 
-app.config['DEBUG'] = False
+#app.config['DEBUG'] = False
 #app.config["MONGO_DBNAME"] = 'dnd_project3'
 #app.config["MONGO_URI"] = 'mongodb+srv://root:r00tuser@myfirstcluster-rxis3.mongodb.net/dnd_project3?retryWrites=true'
 
 # If statement to ensure env vars are set running locally or on heroku
 
-if app.config["DEBUG"] == True:
+if os.environ.get('C9_HOSTNAME'):
     import config
+    app.config['DEBUG'] = True
     app.config['MONGO_URI'] = config.MONGO_URI
     app.config['DB_NAME'] = config.DB_NAME
 else:
+    app.config['DEBUG'] = False
     app.config["MONGO_URI"] = os.getenv("MONGO_URI")
     app.config["DB_NAME"] = os.getenv("DB_NAME")
 
